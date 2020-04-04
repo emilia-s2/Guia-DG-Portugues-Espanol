cdd0693d70bf987740763d0a84cfb7452007d365
emilia-s2 <57104027+emilia-s2@users.noreply.github.com>
2020-04-04 06:21:01 -0300

    Criados

---
+++ README.md
@@ -0,0 +1,50 @@
+TERA-Guide
+======
+
+# 功能简介: 为TERA的常用副本提供[技能]辅助 文字提示
+
+岱魔鲁斯的轮盘 (Demon's Wheel)
+
+拉坎里斯的废墟 (Ruinous Manor)
+
+贝里克神殿 (Velik’s Sanctuary)
+
+RK-9机库 (RK-9 Kennel)
+
+革命团总部 (Red Refuge)
+
+安塔洛斯的深渊 (Antaroth's Abyss)
+
+泰内布利斯城堡 (Dark Reach Citadel)
+
+里安的地下殿堂 (Grotto of Lost Souls)
+
+巴哈勒神殿 (Bahaar's Sanctum)
+
+费尔奎娜巢穴 (Gossamer Vault)
+
+费勒诺的实验室 (Demokron Factory)
+
+伯恩斯坦恶灵岛 (Forsaken Island)
+
+贝尔亚克城堡 (Akalath Quarantine)
+
+金鳞号 (Sailing Instance)
+
+凯尔赛克隐藏地(Corrupted Skynest)
+
+狂气竞技场(Forbidden Arena)
+
+------
+
+/8频道 键入命令 | 效果说明
+--- | ---
+辅助 | 开启/关闭模组
+辅助 主播 | [提示文字] 发送到 代理频道
+
+------
+
+Proxy(/8) Usage (proxy command) | description
+--- | ---
+guide | enabled / disabled
+guide stream(st) | Send To Stream
---
+++ boss-Custom.js
@@ -0,0 +1,141 @@
+// Translation Edition (Custom Edit)
+module.exports = {
+	// Into Dungeon Tip-Msg
+	DungeonInfo : [
+		{zone: 9066, string: `Welcome to <font color="#56B4E9">Demon's Wheel</font>`},
+		{zone: 9059, string: `Welcome to <font color="#56B4E9">Forsaken Island</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9759, string: `Welcome to <font color="#56B4E9">Forsaken Island</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 9767, string: `Welcome to <font color="#56B4E9">Demokron Factory</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9067, string: `Welcome to <font color="#56B4E9">Demokron Factory</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 9770, string: `Welcome to <font color="#56B4E9">Ruinous Manor</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9970, string: `Welcome to <font color="#56B4E9">Ruinous Manor</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 9781, string: `Welcome to <font color="#56B4E9">Velik's Sanctuary</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9981, string: `Welcome to <font color="#56B4E9">Velik's Sanctuary</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 9735, string: `Welcome to <font color="#56B4E9">RK-9 Kennel</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9935, string: `Welcome to <font color="#56B4E9">RK-9 Kennel</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 9739, string: `Welcome to <font color="#56B4E9">Red Refuge</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9939, string: `Welcome to <font color="#56B4E9">Red Refuge</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 9720, string: `Welcome to <font color="#56B4E9">Antaroth's Abyss</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9920, string: `Welcome to <font color="#56B4E9">Antaroth's Abyss</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 3017, string: `Welcome to <font color="#56B4E9">Antaroth's Abyss</font> <font color="#FF0000"> 7-Plays</font>`},
+		{zone: 9783, string: `Welcome to <font color="#56B4E9">Dark Reach Citadel</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9983, string: `Welcome to <font color="#56B4E9">Dark Reach Citadel</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 3018, string: `Welcome to <font color="#56B4E9">Dark Reach Citadel</font> <font color="#FF0000"> 7-Plays</font>`},
+		{zone: 9782, string: `Welcome to <font color="#56B4E9">Grotto of Lost Souls</font> <font color="#E69F00">Normal</font>`},
+		{zone: 9982, string: `Welcome to <font color="#56B4E9">Grotto of Lost Souls</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 3019, string: `Welcome to <font color="#56B4E9">Grotto of Lost Souls</font> <font color="#FF0000"> 7-Plays</font>`},
+		{zone: 9044, string: `Welcome to <font color="#56B4E9">Bahaar's Sanctum</font>`},
+		{zone: 3101, string: `Welcome to <font color="#56B4E9">Gossamer Vault</font> <font color="#E69F00">Normal</font>`},
+		{zone: 3201, string: `Welcome to <font color="#56B4E9">Gossamer Vault</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 3023, string: `Welcome to <font color="#56B4E9">Akalath Quarantine</font>`},
+		{zone: 3020, string: `Welcome to <font color="#56B4E9">Sea of Honor</font>`},
+		{zone: 3026, string: `Welcome to <font color="#56B4E9">Corrupted Skynest</font> <font color="#E69F00">Normal</font>`},
+		{zone: 3126, string: `Welcome to <font color="#56B4E9">Corrupted Skynest</font> <font color="#00FFFF">Hard</font>`},
+		{zone: 3027, string: 'Welcome to <font color="#56B4E9">Forbidden Arena</font>'}
+	],
+	// Demon's Wheel
+	DW_BOSS_1 : [
+	],
+	DW_TipMsg1: [],
+	DW_BOSS_2 : [
+	],
+	DW_TipMsg2: [
+	],
+	// Forsaken Island
+	FI_BOSS_1 : [
+	],
+	FI_BOSS_2 : [
+	],
+	FI_BOSS_3 : [
+	],
+	FI_TipMsg : [],
+	// Demokron Factory
+	DF_BOSS_1 : [
+	],
+	DF_BOSS_2 : [
+	],
+	DF_BOSS_3 : [
+	],
+	// Ruinous Manor
+	RM_BOSS_1 : [
+	],
+	RM_BOSS_2 : [
+	],
+	RM_BOSS_3 : [
+	],
+	// Velik's Sanctuary
+	VS_BOSS_1 : [
+	],
+	VS_BOSS_2 : [
+	],
+	VS_BOSS_3 : [
+	],
+	VS_TipMsg : [],
+	// RK-9 Kennel
+	RK_BOSS_1 : [
+	],
+	RK_BOSS_2 : [
+	],
+	RK_BOSS_3 : [
+	],
+	RK_TipMsg : [],
+	// Red Refuge
+	RR_BOSS_1 : [
+	],
+	RR_BOSS_2 : [
+	],
+	RR_BOSS_3 : [
+	],
+	// Antaroth's Abyss
+	AA_BOSS_1 : [
+	],
+	AA_BOSS_2 : [
+	],
+	AA_BOSS_3 : [
+	],
+	// Dark Reach Citadel
+	DRC_BOSS_1 : [
+	],
+	DRC_BOSS_2 : [
+	],
+	DRC_BOSS_3 : [
+	],
+	DRC_TipMsg : [],
+	// Grotto of Lost Souls
+	GLS_BOSS_1 : [
+	],
+	GLS_BOSS_2 : [
+	],
+	GLS_BOSS_3 : [
+	],
+	// Bahaar's Sanctum
+	BS_BOSS : [
+	],
+	BS_TipMsg : [],
+	// Gossamer Vault
+	GV_BOSS_1 : [
+	],
+	GV_BOSS_2 : [
+	],
+	// Akalath Quarantine
+	AQ_BOSS_1 : [
+	],
+	AQ_BOSS_2 : [
+	],
+	// Sailing Instance / Sea of Honor
+	SI_BOSS_1 : [
+	],
+	SI_BOSS_2 : [
+	],
+	SI_BOSS_3 : [
+	],
+	SI_TipMsg : [],
+	// Corrupted Skynest
+	CK_BOSS : [
+	],
+	CK_TipMsg : [],
+	// Forbidden Arena
+	FA_BOSS : [
+	],
+	FA_TipMsg : []
+}
