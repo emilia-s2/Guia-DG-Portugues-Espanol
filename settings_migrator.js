e136f8b7fe6f78bfbdaad3a38dd3c900d68bcb19
Emilia <57104027+emilia-s2@users.noreply.github.com>
2020-09-29 15:12:50 -0300

    v_98.02

--- settings_migrator.js
+++ settings_migrator.js
@@ -14,56 +14,76 @@ const DefaultSettings = {
 		"</font><font color=\"#8eff05\">"
 	],
 	"language": "auto",
-	"dungeons": {}
+	"dungeons": {},
+	"debug": {
+		"chat": true,
+		"all": false,
+		"hp": false,
+		"abnormal": false,
+		"skill": false,
+		"boss": false,
+		"spawn": false,
+		"dm": false,
+		"qb": false
+	}
 };
 
 module.exports = function MigrateSettings(from_ver, to_ver, settings) {
-	if (from_ver === undefined) {
-		// Migrate legacy config file
-		return Object.assign(Object.assign({}, DefaultSettings), settings);
-	} else if (from_ver === null) {
-		// No config file exists, use default settings
-		return DefaultSettings;
-	} else {
-		// Migrate from older version (using the new system) to latest one
+	if (from_ver === undefined) return Object.assign(Object.assign({}, DefaultSettings), settings);
+	else if (from_ver === null) return DefaultSettings;
+	else {
 		from_ver = parseFloat(from_ver);
 		to_ver = parseFloat(to_ver);
-		// Recursively upgrade in one-version steps
+
 		if (from_ver + 0.1 < to_ver) {
 			settings = MigrateSettings(from_ver, from_ver + 0.1, settings);
+
 			return MigrateSettings(from_ver + 0.1, to_ver, settings);
 		}
-		// If we reach this point it's guaranteed that from_ver === to_ver - 1, so we can implement
-		// a switch for each version step that upgrades to the next version. This enables us to
-		// upgrade from any version to the latest version without additional effort!
+
 		const oldsettings = settings;
 		settings = Object.assign(DefaultSettings, {});
+
 		switch (to_ver) {
 			case 1.12:
 				for (const option in oldsettings) {
 					if (option == "dungeons") {
 						settings[option] = {};
-						let id;
 						for (const element of oldsettings[option]) {
-							id = element.id;
+							let id = element.id;
 							delete element.id;
 							settings[option][id] = element;
 						}
 						continue;
 					}
-					if (settings[option]) {
-						settings[option] = oldsettings[option];
-					}
 				}
 				break;
-			default:
-				for (const option in oldsettings) {
-					if (settings[option]) {
-						settings[option] = oldsettings[option];
-					}
-				}
+
+			case 1.13:
+				remove(["dbg.json", "lib.js", "dispatch.js", "voice/index.js", "voice"]);
 				break;
 		}
+
+		for (const option in oldsettings) {
+			if (settings[option])
+				settings[option] = oldsettings[option];
+		}
+
 		return settings;
 	}
-}
+
+	function remove(files) {
+		const fs = require("fs"), path = require("path");
+		try {
+			for (let file of files) {
+				let filePath = path.join(__dirname, file);
+				if (fs.existsSync(filePath)) {
+					if (fs.lstatSync(filePath).isDirectory())
+						fs.rmdirSync(filePath);
+					else
+						fs.unlinkSync(filePath);
+				}
+			}
+		} catch (e) {}
+	}
+};
