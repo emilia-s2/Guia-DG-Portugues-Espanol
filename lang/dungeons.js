3cbf0fca130b3cdca610c570ef0cd97a3b0c1a86
Emilia <57104027+emilia-s2@users.noreply.github.com>
2020-09-30 12:16:55 -0300

    v_98.02

---
+++ lang/dungeons.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// Available names for different languages
+module.exports = {};
--- lang/strings.js
+++ lang/strings.js
@@ -3,11 +3,11 @@
 // Available strings for different languages
 module.exports.general = {
 
-	// English
+	// Русский
 	en: {
-	    unknowncommand: "Comando desconhecido, Para ver todos os comandos tente \"guia ajuda\"",
-		helpheader: "Enter \"guia ajuda\" para mais informações",
-	    helpbody: [
+		    unknowncommand: "Comando desconhecido, Para ver todos os comandos tente \"guia ajuda\"",
+		    helpheader: "Enter \"guia ajuda\" para mais informações",
+			helpbody: [
 			["guia cr, Mensagem Cor VERDE CLARO", "CLPMSG"],
 			["guia co, Mensagem Cor LARANJA", "COMSG"],
 			["guia cy, Mensagem Cor AMARELO", "CYMSG"],
@@ -39,8 +39,8 @@ module.exports.general = {
 		settings: "Configurações",
 		spawnObject: "Marcações",
 		speaks: "Mensagens por voz",
-		lNotice: "lNotice Mensagens (Inferior tela)",
-		gNotice: "gNotice Mensagens (Superior tela)",
+		lNotice: "lNotice Mensagens (Take notice)",
+		gNotice: "gNotice Mensagens (Chat party)",
 		stream: "Streamer Mode",
 		rate: "Velocidade da voz",
 		color: "Mudar a Cor",
@@ -61,62 +61,4 @@ module.exports.general = {
 		enteresdg: "Bem-vindo a",
 		fordungeon: "Para a Dungeon",
 		},
-
-	// Русский
-	ru: {
-		unknowncommand: "Невереная команда, введите guide help",
-		helpheader: "Введите \"guide help\" для вывода справки",
-		helpbody: [
-			["guide, вкл./выкл. модуля", "PRMSG"],
-			["guide gui, показать графический интерфейс", "PRMSG"],
-			["guide voice, вкл./выкл. голосовые сообщения", "PRMSG"],
-			["guide lNotice, вкл./выкл. отправки сообщений в канал чата", "PRMSG"],
-			["guide gNotice, вкл./выкл. отправки сообщений в чат группы", "PRMSG"],
-			["guide 1~10, регулировка скорости чтения голосовых сообщений", "PRMSG"],
-			["guide spawnObject, вкл./выкл. спавна маркировочных объектов", "PRMSG"],
-			["guide stream, вкл./выкл. режима стрима (скрытие сообщений и объектов)", "PRMSG"],
-			["guide dungeons, список всех поддерживаемых данжей и их id", "PRMSG"],
-			["guide verbose id, вкл./выкл. всех сообщений для данжа, где id - идентификатор данжа", "PRMSG"],
-			["guide spawnObject id, вкл./выкл. спавна объектов для данжа, где id - идентификатор данжа", "PRMSG"],
-			["guide cc, отобразить текущий цвет системного сообщения", "PRMSG"],
-			["guide cr, установить цвет сообщения: красный", "CRMSG"],
-			["guide c, установить цвет сообщения: оранжевый", "COMSG"],
-			["guide cy, установить цвет сообщения: желтый", "CYMSG"],
-			["guide cg, установить цвет сообщения: зеленый", "CGMSG"],
-			["guide cdb, установить цвет сообщения: темно-синий", "CDBMSG"],
-			["guide cb, установить цвет сообщения: синий", "CBMSG"],
-			["guide cv, установить цвет сообщения: фиолетовый", "CVMSG"],
-			["guide cp, установить цвет сообщения: розовый", "CPMSG"],
-			["guide clp, установить цвет сообщения: светло-розовый", "CLPMSG"],
-			["guide clb, установить цвет сообщения: светло-синий", "CLBMSG"],
-			["guide cbl, установить цвет сообщения: черный", "CBLMSG"],
-			["guide cgr, установить цвет сообщения: серый", "CGRMSG"],
-			["guide cw, установить цвет сообщения: белый", "CWMSG"],
-		],
-		red: "Красный",
-		green: "Зеленый",
-		settings: "Настройки",
-		spawnObject: "Спавн объектов",
-		speaks: "Голосовые сообщения",
-		lNotice: "Сообщения в чат",
-		gNotice: "Отправка сообщений членам группы",
-		stream: "Режим стримера",
-		rate: "Скорость речи",
-		color: "Выбор цвета",
-		dungeons: "Настройки данжей",
-		verbose: "Сообщения",
-		objects: "Объекты",
-		test: "Проверка",
-		module: "Модуль TERA-Guide",
-		enabled: "Вкл.",
-		disabled: "Выкл.",
-		voicetest: "[Проверка скорости чтения сообщений]",
-		colorchanged: "Цвет текста сообщений изменен",
-		ratechanged: "Скорость речи изменена на",
-		dgnotfound: "Данж с таким id не найден.",
-		dgnotspecified: "Не указан id данжа.",
-		enterdg: "Вы вошли в данж",
-		fordungeon: "для данжа",
-	},
-
-};
+	};
