74e0d365ad76a0fbd5daf56a3723d1ad2a7341eb
Emilia <57104027+emilia-s2@users.noreply.github.com>
2020-09-30 22:25:46 -0300

    v_98.02

--- lang/dungeons.js
+++ lang/dungeons.js
@@ -1,4 +1,42 @@
 'use strict';
 
-// Available names for different languages
-module.exports = {};
+// Dungeon names
+module.exports = {
+
+    "9781": {
+	en: "Velik's Sanctuary Normal"},
+	"9739": {
+	en: "Red Refuge Normal"},
+	"9735": {
+	en: "RK-9 Kennel Normal"},
+	"3034": {
+	en: "Rampaging RK-9 Kennel"},
+	"9920": {
+	en: "Antaroth's Abyss (Dificil)"},
+	 "9982": {
+	en: "Grotto of Lost Souls (Dificil)"},
+	"9044": {
+	en: "Bahaar's Sanctum"},
+	"3201": {
+	en: "Gossamer Vault (Dificil)"},
+	"3023": {
+    en: "Akalath Quarantine"},
+	"3020": {
+	en: "Sea of Honor"},
+	"3026": {
+	en: "Corrupted Skynest Normal"},
+	"3126": {
+	en: "Corrupted Skynest (Dificil)"},
+	"3102": {
+	en: "Draakon Arena Normal"},
+	"3202": {
+	en: "Draakon Arena (Dificil)"},
+	 "3027": {
+	en: "Forbidden Arena [Hagufna]"},
+	"3103": {
+	en: "Forbidden Arena [Undying Warlord]"},
+	"3203": {
+	en: "Forbidden Arena [Nightmare Undying Warlord]"},
+	"9053": {
+    en: "Kezzel's Gorge (5-Person)"},
+};
