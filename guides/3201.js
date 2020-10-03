40e8fe0c18039cc545e4d7e5b387b90e34c9a3a7
Emilia <57104027+emilia-s2@users.noreply.github.com>
2020-10-03 15:21:40 -0300

    v_98.02

--- guides/3201.js
+++ guides/3201.js
@@ -1,141 +1,69 @@
-// Gossamer Vault (Hard)
+// Gossamer Vault (Dificil)
 //
 // made by michengs
 
 module.exports = (dispatch, handlers, guide, lang) => {
 	let notice = true;
-	let boss = 3;
+	let boss = null;
 	let secondboss = false;
 
 	function secondboss_start_event() {
 		secondboss = true;
 		notice = true;
-		boss = 3;
+		boss = null;
 	}
 
 	function secondboss_debuff_event(skillid) {
-		if (skillid === 203 || skillid === 204) {
-				notice = false;
-				dispatch.setTimeout(() => notice = true, 4000);
-		}
-
-		if (notice && skillid === 234 && boss === 1) { //203 204技能没出/满足234 打手位置本体技能/满足吃分身buff
-			dispatch.setTimeout(() => {
-				handlers.text({
-					sub_type: "alert",
-					message_RU: "Скоро дебафф!",
-					message: "Debuff chegando"
-				});	
-			}, 55000);
-		}
-
-		if (notice && skillid === 234 && boss === 0) { //203 204技能没出/满足234 打手位置本体技能/满足吃本体buff
-			dispatch.setTimeout(() => {
-				handlers.text({
-					sub_type: "alert",
-					message_RU: "Скоро дебафф!",
-					message: "Debuff chegando"
-				});
-			}, 55000);
-		}
-
-		if (skillid === 32010224) {  //吃分身buff
+		if (skillid === 32010224) { // false debuff (next true)
 			boss = 1;
 
 			dispatch.setTimeout(() => {
 				if (boss === 1) {
 					handlers.text({
 						sub_type: "message",
-						message_RU: "Смена дебаффа!",
+						message_RU: "Смена дебаффа",
 						message: "Recarregar Debuff"
 					});
-					boss = 3;
+					boss = null;
 				}
 			}, 80000);
 		}
 
-		if (skillid === 32010220) { //吃本体buff
+		if (skillid === 32010220) { // true debuff (next false)
 			boss = 0;
 
 			dispatch.setTimeout(() => {
 				if (boss === 0){
 					handlers.text({
 						sub_type: "message",
-						message_RU: "Смена дебаффа!",
+						message_RU: "Смена дебаффа",
 						message: "Recarregar Debuff"
 					});
-					boss = 3;
+					boss = null;
 				}
 			}, 80000);
 		}
 
-		if (skillid === 203 && boss === 0) {
+		if ([203, 204].includes(skillid)) {
+			notice = false;
+			dispatch.setTimeout(() => notice = true, 4000);
+
 			dispatch.setTimeout(() => {
 				handlers.text({
 					sub_type: "alert",
-					message_RU: "Скоро дебафф!",
-					message: "Debuff chegando"
+					message_RU: "Скоро дебафф...",
+					message: "Debuff Chegando..."
 				});
 			}, 55000);
 		}
 
-		if (skillid === 203 && boss === 1) {
+		if (notice && skillid === 234) {
 			dispatch.setTimeout(() => {
 				handlers.text({
 					sub_type: "alert",
-					message_RU: "Скоро дебафф!",
-					message: "Debuff chegando"
-				});
-			}, 55000);
-		}
-
-		if (skillid === 204 && boss === 1) {
-			dispatch.setTimeout(() => {
-				handlers.text({
-					sub_type: "alert",
-					message_RU: "Скоро дебафф!",
-					message: "Debuff chegando"
-				});
-			}, 55000);
-		}
-
-		if (skillid === 204 && boss === 0) {
-			dispatch.setTimeout(() => {
-				handlers.text({
-					sub_type: "alert",
-					message_RU: "Скоро дебафф!",
-					message: "Debuff chegando"
-				});
-			}, 55000);
-		}
-
-		if (skillid === 203 && boss === 3) {
-			dispatch.setTimeout(() => {
-				handlers.text({
-					sub_type: "alert",
-					message_RU: "Скоро дебафф!",
-					message: "Debuff chegando"
-				});
-			}, 55000);
-		}
-
-		if (skillid === 204 && boss === 3) {
-			dispatch.setTimeout(() => {
-				handlers.text({
-					sub_type: "alert",
-					message_RU: "Скоро дебафф!",
-					message: "Debuff chegando"
-				});
-			}, 55000);
-		}
-
-		if (notice && skillid === 234 && boss === 3) {
-			dispatch.setTimeout(() => {
-				handlers.text({
-					sub_type: "alert",
-					message_RU: "Скоро дебафф!!!",
-					message: "Debuff chegando"
-				});
+					message_RU: "Скоро дебафф...",
+					message: "Debuff Chegando..."
+				});	
 			}, 55000);
 		}
 	}
@@ -146,29 +74,28 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "stop_timers" },
 			{ type: "despawn_all" }
 		],
-
 		//"s-3201-1000-103-0": [{ type: "text", class_position:"tank", sub_type: "message", message: "Dodge", message_RU: "Эвейд!" }],
 		"s-3201-1000-104-0": [{ type: "text", class_position:"tank", sub_type: "message", message: "Stun Frontal", message_RU: "Стан!" }],
 		"s-3201-1000-107-0": [
-			{ type: "text", sub_type: "message", message: "Empurrar Atras(Lento)", message_RU: "|Полоса|" }
+			{ type: "text", sub_type: "message", message: "Empurrar Atras (Lento)", message_RU: "|Полоса|" }
 //			{ type: "text", sub_type: "message", delay: 2250, message: "pull", message_RU: "Откид!!!" }
 		],
 
 		"s-3201-1000-111-0": [{ type: "text", sub_type: "message", message: "Spray Atras (Lento)", message_RU: "Волна назад" }],
 		"s-3201-1000-112-0": [{ type: "text", sub_type: "message", message: "<-Spray ESQUERDO e DIREITO->", message_RU: "Лево + Право" }],
 		"s-3201-1000-113-0": [
-			{ type: "text", sub_type: "message", message: "Pular (Lento)", message_RU: "Прыжок" },
+			{ type: "text", sub_type: "message", message: "Salto (Lento)", message_RU: "Прыжок" },
 			{ type: "text", sub_type: "message", delay: 1900, message: "Iframe", message_RU: "Камень!" }
 		],
 		"s-3201-1000-118-0": [
-			{ type: "text", sub_type: "message", message: "Pular (Lento)", message_RU: "Прыжок" },
+			{ type: "text", sub_type: "message", message: "Salto (Lento)", message_RU: "Прыжок" },
 			{ type: "text", sub_type: "message", delay: 1900, message: "Iframe", message_RU: "Камень!" },
 		],
 		"s-3201-1000-119-0": [{ type: "text", sub_type: "message", delay: 0, message: "Spray FRENTE e ATRAS", message_RU: "Вперед + Назад" },
-			{ type: "spawn", func: "vector", args: [553, 2, 0, 70, 800, 0, 2500] },  
-			{ type: "spawn", func: "vector", args: [553, 2, 0, 110, 800, 0, 2500] }, 
-			{ type: "spawn", func: "vector", args: [553, 2, 0, 250, 800, 0, 2500] }, 
-			{ type: "spawn", func: "vector", args: [553, 2, 0, 290, 800, 0, 2500] }  
+			{ type: "spawn", func: "vector", args: [553, 2, 0, 70, 800, 0, 2500] },  //Adicionado
+			{ type: "spawn", func: "vector", args: [553, 2, 0, 110, 800, 0, 2500] }, //Adicionado
+			{ type: "spawn", func: "vector", args: [553, 2, 0, 250, 800, 0, 2500] }, //Adicionado
+			{ type: "spawn", func: "vector", args: [553, 2, 0, 290, 800, 0, 2500] }  //Adicionado
 	    ],
 		//"s-3201-1000-121-0": [{ type: "text", class_position:"tank", sub_type: "message", message: "Right", message_RU: "Право" }],
 		//"s-3201-1000-122-0": [{ type: "text", class_position:"tank", sub_type: "message", message: "Left", message_RU: "Лево" }],
@@ -181,20 +108,18 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "text", class_position:"heal", sub_type: "message", message: "Spray Atras (Rapido)", message_RU: "Волна назад (фаст)" }
 		],
 		"s-3201-1000-132-0": [{ type: "text", sub_type: "message", message: "<-Spray ESQUERDO e DIREITO->", message_RU: "Лево + Право (фаст)" }],
-		"s-3201-1000-133-0": [{ type: "text", sub_type: "message", delay: 0, message: "Pular (Rapido)", message_RU: "Прыжок (фаст)" },
-		    { type: "text", sub_type: "message", delay: 900, message: "Iframe", message_RU: "Камень!" }
-	    ],	
-		"s-3201-1000-138-0": [{ type: "text", sub_type: "message", delay: 0, message: "Pular (Rapido)", message_RU: "Прыжок (фаст)" },
-		    { type: "text", sub_type: "message", delay: 900, message: "Iframe", message_RU: "Камень!" }
-	    ],	
+		"s-3201-1000-133-0": [{ type: "text", sub_type: "message", delay: 0, message: "Salto (Rapido)", message_RU: "Прыжок (фаст)" },
+		{ type: "text", sub_type: "message", delay: 900, message: "Iframe", message_RU: "Камень!" }
+		],
+		"s-3201-1000-138-0": [{ type: "text", sub_type: "message", delay: 0, message: "Salto (Rapido)", message_RU: "Прыжок (фаст)" },
+		{ type: "text", sub_type: "message", delay: 900, message: "Iframe", message_RU: "Камень!" }
+		],
 		"s-3201-1000-139-0": [{ type: "text", sub_type: "message", message: "Spray FRENTE e ATRAS (Rapido)", message_RU: "Вперед + Назад (фаст)" },
-		    { type: "spawn", func: "vector", args: [553, 2, 0, 70, 800, 0, 2500] },  
-			{ type: "spawn", func: "vector", args: [553, 2, 0, 110, 800, 0, 2500] }, 
-			{ type: "spawn", func: "vector", args: [553, 2, 0, 250, 800, 0, 2500] }, 
-			{ type: "spawn", func: "vector", args: [553, 2, 0, 290, 800, 0, 2500] }  
+		    { type: "spawn", func: "vector", args: [553, 2, 0, 70, 800, 0, 2500] },  //Adicionado
+			{ type: "spawn", func: "vector", args: [553, 2, 0, 110, 800, 0, 2500] }, //Adicionado
+			{ type: "spawn", func: "vector", args: [553, 2, 0, 250, 800, 0, 2500] }, //Adicionado
+			{ type: "spawn", func: "vector", args: [553, 2, 0, 290, 800, 0, 2500] }  //Adicionado
 	    ],
-		//"s-3201-1000-141-0": [{ type: "text", class_position:"tank", sub_type: "message", message_RU: "双手前砸 (медленно)" }],
-		//"s-3201-1000-142-0": [{ type: "text", class_position:"tank", sub_type: "message", message_RU: "1правая+левая" }],
 /*		"s-3201-1000-143-0": [
 			{ type: "text", class_position:"tank", sub_type: "message", message: "Left > Right", message_RU: "Слева > Справа" },
 			{ type: "text", class_position:"dps", sub_type: "message", message: "Right > Left", message_RU: "Справа > Слева" },
@@ -234,7 +159,6 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 75, 10, 300, 0, 6000] }
 		],
 
-
 		// 2 BOSS
 		"nd-3201-2000": [
 			{ type: "stop_timers" },
@@ -243,45 +167,24 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"h-3201-2000-99": [{ type: "func", func: secondboss_start_event }],
 		"h-3201-2000-81": [{ type: "text", sub_type: "message", message: "80%", message_RU: "Дебафф" }],
 		"h-3201-2000-76": [{ type: "text", sub_type: "message", message: "75%", message_RU: "Камни" }],
-
-		//"s-3201-2000-101-0": [{ type: "text", class_position:"tank", sub_type: "message", message: "right left", message_RU: "右手 左手" }],
-		//"s-3201-2000-102-0": [{ type: "text", class_position:"tank", sub_type: "message", message: "left right", message_RU: "左手 右手" }],
-		//"s-3201-2000-103-0": [{ type: "text", class_position:"tank", sub_type: "message", message: "spin", message_RU: "旋转顺" }],
-		//"s-3201-2000-104-0": [{ type: "text", class_position:"tank", sub_type: "message", message: "right", message_RU: "右手" }],
-		//"s-3201-2000-105-0": [{ type: "text", class_position:"tank", sub_type: "message", message: "front", message_RU: "前突刺" }],
-		//"s-3201-2000-107-0": [{ type: "text", class_position:"tank", sub_type: "message", message: "left", message_RU: "左手" }],
 		"s-3201-2000-108-0": [{ type: "text", sub_type: "message", message: "Ataque Frente | Atras", message_RU: "Откид назад!" }],
-		//"s-3201-2000-109-0": [{ type: "text", class_position:"tank", sub_type: "message", message: "quaternion Attack", message_RU: "4连击" }],
-		//"s-3201-2000-110-0": [{ type: "text", sub_type: "message", message_RU: "?" }],
-		//"s-3201-2000-114-0": [{ type: "text", sub_type: "message", message_RU: "??" }],
-		//"s-3201-2000-116-0": [{ type: "text", sub_type: "message", message: "Back", message_RU: "Назад" }],
-//		"s-3201-2000-150-0": [{ type: "text", sub_type: "message", message: "Phantom", message_RU: "Фантом" }],
-		//"s-3201-2000-201-0": [{ type: "text", sub_type: "message", message: "back 8m", message_RU: "Движение назад 8 м" }],
-		//"s-3201-2000-202-0": [{ type: "text", sub_type: "message", message: "front 8m", message_RU: "Движение вперед 8м" }],
+		"s-3201-2000-150-0": [{ type: "text", sub_type: "message", message: "Fantasma", message_RU: "Фантом" }],
 		"s-3201-2000-203-0": [{ type: "func", func: secondboss_debuff_event, args: [203] }],
 		"s-3201-2000-204-0": [{ type: "func", func: secondboss_debuff_event, args: [204] }],
 
 		"am-3201-320126-32010224": [
-			{ type: "text", sub_type: "alert", delay: 52000, message: "REAL Debuff em 5 segundos", message_RU: "След. правда" },
+			{ type: "text", sub_type: "alert", delay: 52000, message: "REAL Debuff em 5 segundos", message_RU: "След. правда" }, //Alterado
 			{ type: "func", func: secondboss_debuff_event, args: [32010224] }
 		],
 		"am-3201-2000-32010220": [
-			{ type: "text", sub_type: "alert", delay: 52000, message: "FAKE Debuff em 5 segundos", message_RU: "След. ложь" },
+			{ type: "text", sub_type: "alert", delay: 52000, message: "FAKE Debuff em 5 segundos", message_RU: "След. ложь" },   //Alterado
 			{ type: "func", func: secondboss_debuff_event, args: [32010220] }
 		],
-		"ae-0-0-9203100": [{ type: "func", func: secondboss_debuff_event, args: [9203100] }],
-
-		//"s-3201-2000-211-0": [{ type: "text", sub_type: "message", message: "front", message_RU: "???" }],
-		//"s-3201-2000-213-0": [{ type: "text", sub_type: "message", message: "back", message_RU: "????" }],
-		// "s-3201-2000-226-0": [{ type: "text", sub_type: "message", message_RU: "空中吸收 蓄力" }],
 		"s-3201-2000-228-0": [ 
 			{ type: "text", sub_type: "message", message: "Juntar Time", message_RU: "Камни (вместе)!!!" },
 			{ type: "text", sub_type: "message", delay: 4000, message: "Esquiva", message_RU: "Эвейд" }
-		//	{ type: "text", sub_type: "alert", delay: 3500, message: "Dodge", message_RU: "Эвейд" }
-		//	{ type: "text", sub_type: "message", delay: 65000, message: "Dodge", message_RU: "Эвейд" },
 		],
-		//"s-3201-2000-229-0": [{ type: "text", sub_type: "message", message: "3", message_RU: "3 Бомбы" }],
-		"s-3201-2000-230-0": [{ type: "text", sub_type: "message", message: "AOE", message_RU: "AOE!!" }],
+		"s-3201-2000-230-0": [{ type: "text", sub_type: "message", message: "AOE (Explosao)", message_RU: "AOE!!" }],
 
 		"s-3201-2000-231-0": [
 			{ type: "text", sub_type: "message", message: "SAIR", message_RU: "От него" },
@@ -291,20 +194,8 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 300, 0, 3000] },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 3, 1000, 0, 3000] }
 		],
-
-		//"s-3201-2000-233-0": [{ type: "text", sub_type: "message", message: "5", message_RU: "5 бомб" }],
-		
 		"s-3201-2000-234-0": [{ type: "func", func: secondboss_debuff_event, args: [234] }],
 		"s-3201-2000-235-0": [{ type: "text", sub_type: "message", message: "Debuffs", message_RU: "注视2人吃鉴定" }],
 		"s-3201-2000-236-0": [{ type: "text", sub_type: "message", message: "Repelir a Frente", message_RU: "Конус вперед (байт)" }],
-
-		/*"s-3201-320115-203": [{ type: "spawn", func: "marker", args: [false, 0, 0, 100, 3000, true, null] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 15, 125, 0, 3000] }
-		], 	// 	1王水晶位 */
-		//320124-------------302 301
-		/*"s-3201-320120-204": [{ type: "spawn", func: "marker", args: [false, 0, 0, 10, 1100, false, ["Бомба замедленного действия", "Бомба замедленного действия"]] },  //炸弹慢
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 15, 150, 0, 1100] }],
-		"s-3201-320120-205": [{ type: "spawn", func: "marker", args: [false, 0, 0, 10, 1100, false, ["Бомба", "Бомба"]] },  //炸弹
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 15, 150, 0, 1100] }]*/
 	};
 };
