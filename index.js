d76998386e18e680bc0327a3bcb13a3513b44e81
Emilia <57104027+emilia-s2@users.noreply.github.com>
2020-10-07 17:21:09 -0300

    v_98.02

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 module.exports.NetworkMod = function (mod) {
 	try {
 		mod.require["tera-guide-core"].load(mod, {
 			colors: { gui: {}, general: {} }, // color settings
 			command: ["guia"], // module command
-			chat_name: "Guide", // set chat author name for notices
+			chat_name: "Guide-DG", // set chat author name for notices
 		});
 	} catch (e) {
 		mod.error("Warning!\nDepended module \"tera-guide-core\" needed for TERA-Guide is not installed!\nPlease download and install: https://github.com/hsdn/tera-guide-core\n");
