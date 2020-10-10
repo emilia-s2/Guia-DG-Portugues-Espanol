c6ba7808e52041229a76a678c7eb3ba004924ad0
Emilia <57104027+emilia-s2@users.noreply.github.com>
2020-10-10 06:29:51 -0300

    Update (v_98.02)

--- lang/dungeons.js
+++ lang/dungeons.js
@@ -3,44 +3,44 @@
 // Dungeon names
 module.exports = {
 
-    "9781": {
-	en: "Velik's Sanctuary Normal"},
-	"9739": {
-	en: "Red Refuge Normal"},
-	"9735": {
-	en: "RK-9 Kennel Normal"},
-	"3034": {
-	en: "Rampaging RK-9 Kennel"},
-	"9920": {
-	en: "Antaroth's Abyss (Dificil)"},
-	"9982": {
-	en: "Grotto of Lost Souls (Dificil)"},
-	"9044": {
-	en: "Bahaar's Sanctum"},
-	"3201": {
-	en: "Gossamer Vault (Dificil)"},
-	"3023": {
-    en: "Akalath Quarantine"},
-	"3020": {
-	en: "Sea of Honor"},
-	"3026": {
-	en: "Corrupted Skynest Normal"},
-	"3126": {
-	en: "Corrupted Skynest (Dificil)"},
-	"3102": {
-	en: "Draakon Arena"},
-	"3202": {
-	en: "Draakon Arena (Dificil)"},
-	"3027": {
-	en: "Forbidden Arena [Hagufna]"},
-	"3103": {
-	en: "Forbidden Arena [Undying Warlord]"},
-	"3203": {
-	en: "Forbidden Arena [Nightmare Undying Warlord]"},
-	"9053": {
-    en: "Kezzel's Gorge (5-Person)"},
-	"7011": {
-    en: "Blightwood"},
-	"7015": {
-    en: "Balder's Refuge"},
+    9781: {
+	"en": "Velik's Sanctuary Normal"},
+	9739: {
+	"en": "Red Refuge Normal"},
+	9735: {
+	"en": "RK-9 Kennel Normal"},
+	3034: {
+	"en": "Rampaging RK-9 Kennel"},
+	9920: {
+	"en": "Antaroth's Abyss (Dificil)"},
+	9982: {
+	"en": "Grotto of Lost Souls (Dificil)"},
+	9044: {
+	"en": "Bahaar's Sanctum"},
+	3201: {
+	"en": "Gossamer Vault (Dificil)"},
+	3023: {
+    "en": "Akalath Quarantine"},
+	3020: {
+	"en": "Sea of Honor"},
+	3026: {
+	"en": "Corrupted Skynest Normal"},
+	3126: {
+	"en": "Corrupted Skynest (Dificil)"},
+	3102: {
+	"en": "Draakon Arena"},
+	3202: {
+	"en": "Draakon Arena (Dificil)"},
+	3027: {
+	"en": "Forbidden Arena [Hagufna]"},
+	3103: {
+	"en": "Forbidden Arena [Undying Warlord]"},
+	3203: {
+	"en": "Forbidden Arena [Nightmare Undying Warlord]"},
+	9053: {
+    "en": "Kezzel's Gorge (5-Person)"},
+	7011: {
+    "en": "Guardian Blightwood"},
+	7015: {
+    "en": "Guardian Balder's Refuge"},
 };
