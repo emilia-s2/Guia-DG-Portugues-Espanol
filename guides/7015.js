fc79bc27d731f68df280113e53b13732475ea5d1
Emilia <57104027+emilia-s2@users.noreply.github.com>
2020-10-10 09:13:20 -0300

    Update (v_98.02)

--- guides/7015.js
+++ guides/7015.js
@@ -5,14 +5,14 @@
 module.exports = (dispatch, handlers, guide, lang) => {
 	return {
 		// 1 BOSS
-		"nd-7015-1000": [
+		"nd-620-1000": [
 			{ type: "stop_timers" },
 			{ type: "despawn_all" }
 		],
 		"s-620-1000-107-0": [{ type: "text", sub_type: "message", message: "Ataque (Atras)", message_RU: "nao sei russo :c desculpa" }],
 		"s-620-1000-115-0": [{ type: "text", sub_type: "message", message: "Bolas de Fogo", message_RU: "nao sei russo :c desculpa" }],
 		"s-620-1000-127-0": [{ type: "text", sub_type: "message", message: "Salto Atras", message_RU: "nao sei russo :c desculpa" },
-			{ type: "spawn", func: "circle", args: [false, 553, 180, 376, 12, 232, 0, 6750] }
+			{ type: "spawn", func: "circle", args: [false, 553, 180, 376, 12, 232, 0, 5000] }
 		],	
 		"s-620-1000-120-0": [{ type: "text", sub_type: "message", message: "Onda de Fogo", message_RU: "nao sei russo :c desculpa" }],
 		"s-620-1000-121-0": [{ type: "text", sub_type: "message", message: "Repelir (iframe)", message_RU: "nao sei russo :c desculpa" }],
@@ -25,10 +25,11 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-620-1000-125-0": [{ type: "text", sub_type: "message", message: "Bolas de Fogo", message_RU: "nao sei russo :c desculpa" }],
 		
 		"s-620-1001-303-0": [{ type: "text", sub_type: "message", message: "AOE", message_RU: "nao sei russo :c desculpa" }],
-		"s-620-1002-303-0": [{ type: "text", sub_type: "message", message: "AOE", message_RU: "nao sei russo :c desculpa" }],
-		"s-620-1003-303-0": [{ type: "text", sub_type: "message", message: "AOE", message_RU: "nao sei russo :c desculpa" }],
-		"s-620-1004-303-0": [{ type: "text", sub_type: "message", message: "AOE", message_RU: "nao sei russo :c desculpa" }],
-		"s-620-1005-303-0": [{ type: "text", sub_type: "message", message: "AOE", message_RU: "nao sei russo :c desculpa" }],
+		"s-620-1002-303-0": [{ type: "alias", id: "s-620-1001-303-0" }],
+		"s-620-1003-303-0": [{ type: "alias", id: "s-620-1001-303-0" }],
+		"s-620-1004-303-0": [{ type: "alias", id: "s-620-1001-303-0" }],
+		"s-620-1005-303-0": [{ type: "alias", id: "s-620-1001-303-0" }],
+		
 		"s-620-1000-129-0": [{ type: "text", sub_type: "message", message: "Bolas de Fogo", message_RU: "nao sei russo :c desculpa" }],
 		"s-620-1000-106-0": [{ type: "text", sub_type: "message", message: "Salto Aleatorio", message_RU: "nao sei russo :c desculpa" }],
 
