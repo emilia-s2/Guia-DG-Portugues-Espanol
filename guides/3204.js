2a085ba37d1fd236e2d5af20456da7c67c848b75
Emilia <57104027+emilia-s2@users.noreply.github.com>
2021-01-31 07:05:11 -0300

    Update (v_102)

--- guides/3204.js
+++ guides/3204.js
@@ -72,7 +72,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3204-1000-119-0": [{ type: "text", sub_type: "message", message: "Two Strikes", message_PT: "Dois Golpes" }],
 		"s-3204-1000-120-0": [
 			{ type: "text", sub_type: "message", message: "Two Strikes + Stun (AOE)", message_PT: "Explosão Ampla Stun" },
-			{ type: "text", sub_type: "message", message: "Stun (AOE)", message_PT: "Iframe", delay: 2250 },
+			{ type: "text", sub_type: "message", message: "Stun (AOE)", message_PT: "Iframe", delay: 2050 },
 			{ type: "spawn", func: "circle", args: [true, 553, 180, -100, 10, 700, 0, 2500], delay: 1500 }
 		],
 		"s-3204-1000-123-0": [{ type: "text", sub_type: "message", message: "Spin", message_PT: "Giro" }],
