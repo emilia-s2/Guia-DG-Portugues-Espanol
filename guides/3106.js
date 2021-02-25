12914e4e1dbc9673ac66dbdea85b747b7da2e9f1
Emilia <57104027+emilia-s2@users.noreply.github.com>
2021-02-25 10:36:33 -0300

    Update (v_103)

--- guides/3106.js
+++ guides/3106.js
@@ -15,17 +15,12 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"qb-3106-1000-32061001": [{ type: "text", sub_type: "message", message: "Debuff (Close)", message_PT: "Debuff (Fechar)" }],
 		"qb-3106-1000-32061002": [{ type: "text", sub_type: "message", message: "Debuff (Furthest)", message_PT: "Debuff (Mais Distante)" }],
 
-		"s-3106-1000-102-0": [
-			{ type: "func", func: () => combo_start = true },
-			{ type: "func", func: () => combo_start = false, delay: 4000 }
-		],
-		// "s-3106-1000-105-0": [{ type: "text", sub_type: "message", message: "Knockback Spin", message_PT: "Оборот (откид)", check_func: () => combo_start === true }],
 		"s-3106-1000-106-0": [{ type: "text", sub_type: "message", message: "Knockback Spin", message_PT: "Giro Atrás (Empurrão)" }],
 		"s-3106-1000-108-0": [{ type: "text", sub_type: "message", message: "Knockback Spin", message_PT: "Giro Atrás (Empurrão)" }],
 		"s-3106-1000-109-0": [{ type: "text", sub_type: "message", message: "Jump (Knockdown)", message_PT: "Salto (Derrubar)" }],
 		"s-3106-1000-201-0": [{ type: "text", sub_type: "message", message: "Front", message_PT: "Frente" }],
-		"s-3106-1000-202-0": [{ type: "text", sub_type: "message", message: "Front", message_PT: "Frente" }],
-		"s-3106-1000-203-0": [{ type: "text", sub_type: "message", message: "Front + Wave", message_PT: "Frente + Onda" }],
+		"s-3106-1000-202-0": [{ type: "text", sub_type: "message", message: "Front", message_PT: "Frente AoE" }],
+		"s-3106-1000-203-0": [{ type: "text", sub_type: "message", message: "Front + Wave", message_PT: "Frente AoE + Onda" }],
 		"s-3106-1000-205-0": [{ type: "text", sub_type: "message", message: "Spin", message_PT: "Giro" }],
 		"s-3106-1000-206-0": [{ type: "text", sub_type: "message", message: "Spin", message_PT: "Giro" }],
 		"s-3106-1000-207-0": [{ type: "text", sub_type: "message", message: "Spin", message_PT: "Giro" }],
@@ -41,18 +36,15 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3106-1000-216-0": [{ type: "text", sub_type: "message", message: "Somersault", message_PT: "Salto Mortal" }],
 		"s-3106-1000-508-0": [
 			{ type: "text", sub_type: "message", message: "Waves Outward", message_PT: "Ondas DENTRO" },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 157, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 307, 1500, 5000] }
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 18, 180, 1500, 5000] },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 12, 350, 1500, 5000] }
 		],
 		"s-3106-1000-509-0": [
 			{ type: "text", sub_type: "message", message: "Waves Inward", message_PT: "Ondas FORA" },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 157, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 307, 1500, 5000] }
+//			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 18, 180, 1500, 5000] },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 12, 350, 1500, 5000] }
 		],
-		"s-3106-1000-506-0": [{ type: "text", sub_type: "message", message: "Bait (Stun)", message_PT: "Bait (Stun)" }],
 		"s-3106-1000-507-0": [{ type: "text", sub_type: "message", message: "Leash + Jump (Knockdown)", message_PT: "Puxar + Salto (Derrubar)" }],
-		// "s-3106-1000-516-0": "s-3106-1000-508-0",
-		// "s-3106-1000-517-0": "s-3106-1000-509-0",
 
 		"s-3106-1000-518-0": [{ type: "text", sub_type: "message", message: "Unleash", message_PT: "Liberar" }],
 		"s-3106-1000-519-0": [{ type: "text", sub_type: "message", message: "Unleash", message_PT: "Liberar" }],
