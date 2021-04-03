2179f2f25c88e02278e9f9f5de0fa62986b0135c
Emilia <57104027+emilia-s2@users.noreply.github.com>
2021-04-03 10:12:24 -0300

    Update (v_104)

--- guides/9070.js
+++ guides/9070.js
@@ -1,6 +1,6 @@
 ﻿// Manglemire
 //
-// made by michengs
+// made by michengs / HSDN / ambushing
 
 module.exports = (dispatch, handlers, guide, lang) => {
 	guide.type = SP;
@@ -9,32 +9,16 @@ module.exports = (dispatch, handlers, guide, lang) => {
 	let count = -1;
 	let shining = true;
 
-	const bossActions = {
-		213: { truth: "Truth | Break shield", lie: "Lie | Puddles (run away)" }, // "My shield will save me!" (shield)
-		212: { truth: "Truth | Out", lie: "Lie | In" }, // "I will kill you all!" (aoe around boss)
-		218: { truth: "Truth | Out", lie: "Lie | In" } // "One of you must die!" (aoe around player)
-	};
-
-	const bossActions_PT = {
-		213: { truth: "Verdade - Quebre o Escudo", lie: "Mentira - Poças (Fugir)" }, // "My shield will save me!" (shield)
-		212: { truth: "Verdade - Sair", lie: "Mentira - Entrar" }, // "I will kill you all!" (aoe around boss)
-		218: { truth: "Verdade - Sair", lie: "Mentira - Entrar" } // "One of you must die!" (aoe around player)
+	const debuffs_hand = {
+		470046: 3,
+		470047: 6,
+		470048: 9
 	};
 
 	function start_boss_event() {
 		bossBuffs = [];
 		count = -1;
 		shining = true;
-	}
-
-	function boss_message_event(skillid) {
-		if ([213, 212, 218].includes(skillid)) {
-			handlers.text({
-				sub_type: "message",
-				message: is_telling_truth() ? bossActions[skillid].truth : bossActions[skillid].lie,
-				message_PT: is_telling_truth() ? bossActions_PT[skillid].truth : bossActions_PT[skillid].lie
-			});
-		}
 	}
 
 	function is_telling_truth() {
@@ -48,12 +32,6 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		return true;
 	}
 
-	const debuffs_hand = {
-		470046: 3,
-		470047: 6,
-		470048: 9
-	};
-
 	const abnormality_change = (added, event) => {
 		if (debuffs_hand[event.id]) {
 			if (added) {
@@ -62,6 +40,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 				}
 			} else {
 				const index = bossBuffs.indexOf(debuffs_hand[event.id]);
+
 				if (index > -1) {
 					bossBuffs.splice(index, 1);
 				}
@@ -69,7 +48,12 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		}
 	};
 
-	const gage_change = (added, event) => {
+	dispatch._mod.dispatch.addOpcode("S_DUNGEON_EVENT_GAGE", 60350);
+
+	dispatch.hook("S_ABNORMALITY_BEGIN", 4, abnormality_change.bind(null, true));
+	dispatch.hook("S_ABNORMALITY_END", 1, abnormality_change.bind(null, false));
+
+	dispatch.hook("S_DUNGEON_EVENT_GAGE", 1, () => {
 		if (shining) {
 			if (count === 100) {
 				count = -1;
@@ -80,13 +64,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 
 			dispatch.setTimeout(() => shining = true, 500);
 		}
-	};
-
-	dispatch._mod.dispatch.addOpcode("S_DUNGEON_EVENT_GAGE", 60350);
-
-	dispatch.hook("S_ABNORMALITY_BEGIN", 4, abnormality_change.bind(null, true));
-	dispatch.hook("S_ABNORMALITY_END", 1, abnormality_change.bind(null, false));
-	dispatch.hook("S_DUNGEON_EVENT_GAGE", 1, gage_change.bind(null, true));
+	});
 
 	return {
 		"ns-470-1000": [{ type: "func", func: start_boss_event }],
@@ -96,15 +74,30 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-470-1000-1120-0": [{ type: "text", sub_type: "message", message: "Pull", message_PT: "Puxar" }],
 		"s-470-1000-1114-0": [{ type: "text", sub_type: "message", message: "Spray", message_PT: "Spray" }],
 		"s-470-1000-1201-0": [{ type: "text", sub_type: "message", message: "Stun", message_PT: "Stun" }],
-		"s-470-1000-1307-0": [{ type: "text", sub_type: "message", message: "Bomb", message_PT: "Bomba" }],
+		"s-470-1000-1307-0": [{ type: "text", sub_type: "message", message: "With a Bomb - Go away. No Bomb - Enter the circle", message_PT: "Uma Bomba - Sair  |  Sem Bomba - Entre no Círculo" }],
 		"s-470-1000-2105-0": [{ type: "text", sub_type: "message", message: "Jump", message_PT: "Salto" }],
 		"s-470-1000-2106-0": [{ type: "text", sub_type: "message", message: "Smash", message_PT: "Esmagar" }],
 		"s-470-1000-2107-0": [{ type: "text", sub_type: "message", message: "Stun", message_PT: "Stun" }],
 		"s-470-1000-2114-0": [{ type: "text", sub_type: "message", message: "Line", message_PT: "Linha" }],
 		"s-470-1000-3106-0": [{ type: "text", sub_type: "message", message: "100" }],
 
-		"s-470-1000-3213-0": [{ type: "func", func: boss_message_event, args: [213] }],
-		"s-470-1000-3212-0": [{ type: "func", func: boss_message_event, args: [212] }],
-		"s-470-1000-3218-0": [{ type: "func", func: boss_message_event, args: [218] }]
+		"s-470-1000-3213-0": [ // "My shield will save me!" (shield)
+			{ type: "text", sub_type: "message", message: "Break shield", message_PT: "Quebrar Escudo", check_func: () => is_telling_truth() },
+			{ type: "text", sub_type: "message", message: "Puddles (run away)", message_PT: "Poças (Fugir)", check_func: () => !is_telling_truth() }
+		],
+		"s-470-1000-3212-0": [ // "I will kill you all!" (aoe around boss)
+			{ type: "text", sub_type: "message", message: "Out", message_PT: "Sair", check_func: () => is_telling_truth() },
+			{ type: "text", sub_type: "message", message: "In", message_PT: "Entrar", check_func: () => !is_telling_truth() }
+		],
+		"s-470-1000-3218-0": [ // "One of you must die!" (aoe around player)
+			{ type: "text", sub_type: "message", message: "Out", message_PT: "Sair", check_func: () => is_telling_truth() },
+			{ type: "text", sub_type: "message", message: "In", message_PT: "Entrar", check_func: () => !is_telling_truth() }
+		],
+
+		"qb-470-1000-470019": [
+			{ type: "text", sub_type: "alert", message: "Truth", message_PT: "Verdade", check_func: () => is_telling_truth() },
+			{ type: "text", sub_type: "alert", message: "Lie", message_PT: "Mentira", check_func: () => !is_telling_truth() }
+		],
+		"qb-470-1000-470011": [{ type: "text", sub_type: "message", message: "Stand on stars", message_PT: "Fique nas Estrelas" }]
 	};
 };
