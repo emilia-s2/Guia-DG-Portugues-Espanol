42d5eab6b8bea8f37278c48103a68e5639ccb459
Emilia <57104027+emilia-s2@users.noreply.github.com>
2021-08-07 19:54:43 -0300

    Update

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 module.exports.NetworkMod = function(mod) {
 	try {
 		mod.require["tera-guide-core"].load(mod, {
+			languages: ["es", "pt", "en"], // supported languages
 			colors: { gui: {}, general: {} }, // color settings
 			command: ["guia"], // module command
 			chat_name: "Guide-DG", // set chat author name for notices
 		});
-		mod.settings.language = "pt";
 	} catch (e) {
 		mod.error("Aviso!\Dependência módulo \"tera-guide-core\" necessário para o TERA-Guide não está instalado!\nPor favor faça o download e instale: https://github.com/hsdn/tera-guide-core\n");
 		throw e;
