693b8f32a0389f9d41bcde6720de26972615e44b
Emilia <57104027+emilia-s2@users.noreply.github.com>
2022-03-10 06:08:48 -0300

    Update (v_114.2)

--- guides/3109.js
+++ guides/3109.js
@@ -2,7 +2,17 @@
 //
 // made by icebrog
 
+const Vec3 = require("tera-vec3");
+
 module.exports = (dispatch, handlers, guide, lang) => {
+
+	function cage_event() {
+		const entity = {};
+		entity.loc = new Vec3(4907, 164061, 4244);
+		entity.loc.w = 1.2419491953919122;
+
+		handlers.spawn({ func: "circle", args: [false, 553, 0, 0, 8, 650, 0, 15000], tag: "pre-cage" }, entity);
+	}
 
 	return {
 		// 1 BOSS
@@ -18,108 +28,122 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		//
 		"s-3109-2000-403-0": [
-			{ type: "text", sub_type: "message", message: "Back Slam", message_PT: "Ataque Atrás´", message_ES: "Golpe Atrás", delay: 2500 },
-			{ type: "spawn", func: "circle", args: [false, 553, 160, 400, 12, 300, 0, 1500], delay: 3500 },
-			{ type: "spawn", func: "circle", args: [false, 553, 200, 400, 12, 300, 0, 1500], delay: 3500 }
+			{ type: "text", sub_type: "message", message: "Back Slam", message_PT: "Ataque Atrás", message_ES: "Ataque Atrás", delay: 2500 },
+			{ type: "spawn", func: "circle", args: [false, 553, 157, 400, 8, 350, 0, 2500], delay: 2500 },
+			{ type: "spawn", func: "circle", args: [false, 553, 203, 400, 8, 350, 0, 2500], delay: 2500 }
 		],
 		"s-3109-2000-405-0": [
 			{ type: "text", sub_type: "message", message: "Front Sweep | Back Slam", message_PT: "Ataque Frontal | Atrás", message_ES: "Ataque Frontal | Atrás" },
-			{ type: "spawn", func: "circle", args: [false, 553, 160, 400, 12, 300, 0, 1500], delay: 2000 },
-			{ type: "spawn", func: "circle", args: [false, 553, 200, 400, 12, 300, 0, 1500], delay: 2000 }
+			{ type: "spawn", func: "circle", args: [false, 553, 157, 400, 8, 350, 0, 2000], delay: 1500 },
+			{ type: "spawn", func: "circle", args: [false, 553, 203, 400, 8, 350, 0, 2000], delay: 1500 }
 		],
 		"s-3109-2000-406-0": [{ type: "text", sub_type: "message", message: "Back Stun", message_PT: "Stun Atrás", message_ES: "Stun Atrás", delay: 1000 }],
 		"s-3109-2000-409-0": [
-			{ type: "text", sub_type: "message", message: "Back Slam", message_PT: "Ataque Atrás", message_ES: "Golpe Atrás", delay: 3500 },
-			{ type: "spawn", func: "circle", args: [false, 553, 160, 425, 12, 250, 0, 1500], delay: 3500 },
-			{ type: "spawn", func: "circle", args: [false, 553, 200, 425, 12, 250, 0, 1500], delay: 3500 }
+			{ type: "text", sub_type: "message", message: "Back Slam", message_PT: "Ataque Atrás", message_ES: "Ataque Atrás", delay: 3000 },
+			{ type: "spawn", func: "circle", args: [false, 553, 157, 400, 12, 225, 0, 2000], delay: 3000 },
+			{ type: "spawn", func: "circle", args: [false, 553, 203, 400, 12, 225, 0, 2000], delay: 3000 }
 		],
 		"s-3109-2000-410-0": [
 			{ type: "text", sub_type: "message", message: "Back Stun", message_PT: "Stun Atrás", message_ES: "Stun Atrás" },
-			{ type: "spawn", func: "circle", args: [false, 553, 160, 425, 12, 250, 0, 1500] },
-			{ type: "spawn", func: "circle", args: [false, 553, 200, 425, 12, 250, 0, 1500] }
+			{ type: "spawn", func: "circle", args: [false, 553, 157, 400, 12, 225, 0, 1500] },
+			{ type: "spawn", func: "circle", args: [false, 553, 203, 400, 12, 225, 0, 1500] }
 		],
-		"s-3109-2000-411-0": [{ type: "text", sub_type: "message", message: "Knockup!", message_PT: "Derrubar", message_ES: "Tumbar!" }],
+		"s-3109-2000-411-0": [
+			{ type: "text", sub_type: "message", message: "Back Knockup!", message_PT: "Derrubar Atrás", message_ES: "Tumbar Atrás" },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 85, 550, 0, 3500] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 270, 550, 0, 3500] }
+		],
 		// In - Out
 		"s-3109-2000-412-0": [
-			{ type: "text", sub_type: "message", message: "Spin | Pushback", message_PT: "Girar | Empurrar", message_ES: "Girar | Empujar" },
-			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 400, 0, 3000] }
+			{ type: "text", sub_type: "message", message: "Pushback", message_PT: "Empurrar Atrás", message_ES: "Empujar Atrás" },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 420, 0, 3000] }
 		],
-		"s-3109-2000-414-0": [
-			{ type: "text", sub_type: "message", message: "Spin | Out-In", message_PT: "Girar | Sair - Entrar", message_ES: "Girar | Salir - Entrar" },
-			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 400, 0, 5000] }
-		],
-		"s-3109-2000-415-0": "s-3109-2000-414-0",
 		"s-3109-2000-416-0": [
-			{ type: "text", sub_type: "message", message: "Spin | Out-In | Pizza", message_PT: "Girar | Sair - Entrar | Pizza", message_ES: "Girar | Salir - Entrar | Pizza" },
-			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 400, 0, 6500] },
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 10, 425, 0, 6500] },
-			{ type: "spawn", func: "marker", args: [false, 32.5, 200, 0, 1500, false], delay: 5000 }, // safe 2
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 55, 425, 0, 6500] },
-			{ type: "spawn", func: "marker", args: [false, 77.5, 200, 0, 5000, false] }, // safe 1
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 100, 425, 0, 6500] },
-			{ type: "spawn", func: "marker", args: [false, 122.5, 200, 0, 1500, false], delay: 5000 }, // safe 2
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 145, 425, 0, 6500] },
-			{ type: "spawn", func: "marker", args: [false, 167.5, 200, 0, 5000, false] }, // safe 1
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 190, 425, 0, 6500] },
-			{ type: "spawn", func: "marker", args: [false, 212.5, 200, 0, 1500, false], delay: 5000 }, // safe 2
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 235, 425, 0, 6500] },
-			{ type: "spawn", func: "marker", args: [false, 257.5, 200, 0, 5000, false] }, // safe 1
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 280, 425, 0, 6500] },
-			{ type: "spawn", func: "marker", args: [false, 302.5, 200, 0, 1500, false], delay: 5000 }, // safe 2
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 325, 425, 0, 6500] },
-			{ type: "spawn", func: "marker", args: [false, 347.5, 200, 0, 5000, false] } // safe 1
+			{ type: "text", sub_type: "message", message: "Out-In | Pizza", message_PT: "Sair - Entrar | Pizza", message_ES: "Salir - Entrar | Pizza" },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 420, 0, 6500] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 7.5, 420, 0, 6500] },
+			{ type: "spawn", func: "marker", args: [false, 30, 200, 0, 1500, false], delay: 5000 },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 52.5, 420, 0, 6500] },
+			{ type: "spawn", func: "marker", args: [false, 75, 200, 0, 5000, false] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 97.5, 420, 0, 6500] },
+			{ type: "spawn", func: "marker", args: [false, 120, 200, 0, 1500, false], delay: 5000 },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 142.5, 420, 0, 6500] },
+			{ type: "spawn", func: "marker", args: [false, 165, 200, 0, 5000, false] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 187.5, 420, 0, 6500] },
+			{ type: "spawn", func: "marker", args: [false, 210, 200, 0, 1500, false], delay: 5000 },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 232.5, 420, 0, 6500] },
+			{ type: "spawn", func: "marker", args: [false, 255, 200, 0, 5000, false] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 277.5, 420, 0, 6500] },
+			{ type: "spawn", func: "marker", args: [false, 300, 200, 0, 1500, false], delay: 5000 },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 322.5, 420, 0, 6500] },
+			{ type: "spawn", func: "marker", args: [false, 345, 200, 0, 5000, false] }
 		],
 		"s-3109-2000-417-0": [
-			{ type: "text", sub_type: "message", message: "Spin | In-Out | Pizza", message_PT: "Girar | Entrar - Sair | Pizza", message_ES: "Girar | Entrar - Salir | Pizza" },
-			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 400, 0, 7500] },
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 10, 425, 0, 7500] },
-			{ type: "spawn", func: "marker", args: [false, 32.5, 200, 0, 6000, false] }, // safe 1
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 55, 425, 0, 7500] },
-			{ type: "spawn", func: "marker", args: [false, 77.5, 200, 0, 1500, false], delay: 6000 }, // safe 2
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 100, 425, 0, 7500] },
-			{ type: "spawn", func: "marker", args: [false, 122.5, 200, 0, 6000, false] }, // safe 1
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 145, 425, 0, 7500] },
-			{ type: "spawn", func: "marker", args: [false, 167.5, 200, 0, 1500, false], delay: 6000 }, // safe 2
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 190, 425, 0, 7500] },
-			{ type: "spawn", func: "marker", args: [false, 212.5, 200, 0, 6000, false] }, // safe 1
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 235, 425, 0, 7500] },
-			{ type: "spawn", func: "marker", args: [false, 257.5, 200, 0, 1500, false], delay: 6000 }, // safe 2
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 280, 425, 0, 7500] },
-			{ type: "spawn", func: "marker", args: [false, 302.5, 200, 0, 6000, false] }, // safe 1
-			{ type: "spawn", func: "vector", args: [553, 0, 20, 325, 425, 0, 7500] },
-			{ type: "spawn", func: "marker", args: [false, 347.5, 200, 0, 1500, false], delay: 6000 } // safe 2
+			{ type: "text", sub_type: "message", message: "In-Out | Pizza", message_PT: "Entrar - Sair | Pizza", message_ES: "Entrar - Salir | Pizza" },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 420, 0, 7500] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 7.5, 420, 0, 7500] },
+			{ type: "spawn", func: "marker", args: [false, 30, 200, 0, 6000, false] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 52.5, 420, 0, 7500] },
+			{ type: "spawn", func: "marker", args: [false, 75, 200, 0, 1500, false], delay: 6000 },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 97.5, 420, 0, 7500] },
+			{ type: "spawn", func: "marker", args: [false, 120, 200, 0, 6000, false] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 142.5, 420, 0, 7500] },
+			{ type: "spawn", func: "marker", args: [false, 165, 200, 0, 1500, false], delay: 6000 },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 187.5, 420, 0, 7500] },
+			{ type: "spawn", func: "marker", args: [false, 210, 200, 0, 6000, false] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 232.5, 420, 0, 7500] },
+			{ type: "spawn", func: "marker", args: [false, 255, 200, 0, 1500, false], delay: 6000 },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 277.5, 420, 0, 7500] },
+			{ type: "spawn", func: "marker", args: [false, 300, 200, 0, 6000, false] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 322.5, 420, 0, 7500] },
+			{ type: "spawn", func: "marker", args: [false, 345, 200, 0, 1500, false], delay: 6000 }
 		],
 		// Dig
+		"s-3109-2000-511-0": [
+			{ type: "text", sub_type: "message", message: "Dodge", message_PT: "Iframe", message_ES: "Iframe" },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 18, 200, 0, 2500] }
+		],
 		"s-3109-2000-602-0": [
 			{ type: "text", sub_type: "message", message: "Get Out", message_PT: "Sair", message_ES: "Sal de Ahí" },
-			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 600, 0, 4000] }
+			{ type: "func", func: cage_event }
 		],
 		"s-3109-2000-604-0": [
+			{ type: "despawn_all", tag: "pre-cage" },
 			{ type: "text", sub_type: "message", message: "Cage", message_PT: "Prisão", message_ES: "Jaula" },
-			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 400, 0, 10000] }
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 405, 0, 15000], tag: "cage" }
 		],
 		"s-3109-2000-605-0": [
-			{ type: "text", sub_type: "message", message: "Pizza | Out-in", message_PT: "Pizza | Salir - Entrar", message_ES: "Pizza | Salir - Entrar", delay: 2000 },
-			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 400, 0, 10000] },
-			{ type: "spawn", func: "vector", args: [553, 0, 0, 22.5, 425, 0, 10000] },
-			{ type: "spawn", func: "marker", args: [false, 45, 200, 0, 8000, false] }, // safe 1
-			{ type: "spawn", func: "vector", args: [553, 0, 0, 67.5, 425, 0, 10000] },
-			{ type: "spawn", func: "marker", args: [false, 90, 200, 0, 750, false], delay: 8000 }, // safe 2
-			{ type: "spawn", func: "vector", args: [553, 0, 0, 112.5, 425, 0, 10000] },
-			{ type: "spawn", func: "marker", args: [false, 135, 200, 0, 8000, false] }, // safe 1
-			{ type: "spawn", func: "vector", args: [553, 0, 0, 157.5, 425, 0, 10000] },
-			{ type: "spawn", func: "marker", args: [false, 180, 200, 0, 750, false], delay: 8000 }, // safe 2
-			{ type: "spawn", func: "vector", args: [553, 0, 0, 202.5, 425, 0, 10000] },
-			{ type: "spawn", func: "marker", args: [false, 225, 200, 0, 8000, false] }, // safe 1
-			{ type: "spawn", func: "vector", args: [553, 0, 0, 247.5, 425, 0, 10000] },
-			{ type: "spawn", func: "marker", args: [false, 270, 200, 0, 750, false], delay: 8000 }, // safe 2
-			{ type: "spawn", func: "vector", args: [553, 0, 0, 292.5, 425, 0, 10000] },
-			{ type: "spawn", func: "marker", args: [false, 315, 200, 0, 8000, false] }, // safe 1
-			{ type: "spawn", func: "vector", args: [553, 0, 0, 337.5, 425, 0, 10000] },
-			{ type: "spawn", func: "marker", args: [false, 360, 200, 0, 750, false], delay: 8000 } // safe 2
+			{ type: "text", sub_type: "message", message: "Pizza | Out-in", message_PT: "Pizza | Sair - Entrar", message_ES: "Pizza | Salir - Entrar", delay: 2000 },
+			{ type: "text", sub_type: "message", message: "Pizza | Out-in", message_PT: "Pizza | Sair - Entrar", message_ES: "Pizza | Salir - Entrar", delay: 5000 },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 407, 0, 10000] },
+			{ type: "despawn_all", tag: "cage" },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 22.5, 407, 0, 10000] },
+			{ type: "spawn", func: "marker", args: [false, 45, 200, 0, 8000, false] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 67.5, 407, 0, 10000] },
+			{ type: "spawn", func: "marker", args: [false, 90, 200, 0, 750, false], delay: 8000 },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 112.5, 407, 0, 10000] },
+			{ type: "spawn", func: "marker", args: [false, 135, 200, 0, 8000, false] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 157.5, 407, 0, 10000] },
+			{ type: "spawn", func: "marker", args: [false, 180, 200, 0, 750, false], delay: 8000 },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 202.5, 407, 0, 10000] },
+			{ type: "spawn", func: "marker", args: [false, 225, 200, 0, 8000, false] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 247.5, 407, 0, 10000] },
+			{ type: "spawn", func: "marker", args: [false, 270, 200, 0, 750, false], delay: 8000 },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 292.5, 407, 0, 10000] },
+			{ type: "spawn", func: "marker", args: [false, 315, 200, 0, 8000, false] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 337.5, 407, 0, 10000] },
+			{ type: "spawn", func: "marker", args: [false, 360, 200, 0, 750, false], delay: 8000 }
 		],
 		// Waves
 		"s-3109-2000-616-0": [{ type: "text", sub_type: "message", message: "Inward Wave (Gather)", message_PT: "Onda (Juntar)", message_ES: "Ola (Juntarse)" }],
-		"s-3109-2000-617-0": [{ type: "text", sub_type: "message", message: "Front | Back Waves", message_PT: "Ataque Frontal | Ondas Atrás", message_ES: "Ataque Frontal | Olas Atrás" }]
+		"ae-0-0-32092005": [{ type: "text", sub_type: "message", message: "Dodge", message_PT: "Iframe", message_ES: "Iframe" }],
+		"s-3109-2000-617-0": [
+			{ type: "text", sub_type: "message", message: "Front | Back Waves", message_PT: "Frente | Ondas Atrás", message_ES: "Frente | Olas Atrás" },
+			{ type: "text", sub_type: "message", message: "Front | Back Waves", message_PT: "Frente | Ondas Atrás", message_ES: "Frente | Olas Atrás", delay: 3000 },
+			{ type: "spawn", func: "vector", args: [553, 270, -950, 165, 1000, 0, 12000] },
+			{ type: "spawn", func: "vector", args: [553, 270, 950, -10, 1000, 0, 12000] },
+			{ type: "spawn", func: "vector", args: [553, 270, -950, 10, 1000, 0, 12000] },
+			{ type: "spawn", func: "vector", args: [553, 270, 950, -165, 1000, 0, 12000] }
+		]
 	};
 };
