fcecafda2b6d5babdffaf6ca6e5d56032e9a9e03
Emilia <emilia.sz_@hotmail.com>
2023-07-23 18:17:45 -0300

    Update Red Refuge Hard/Normal (23/07/23)

--- guides/9739.js
+++ guides/9739.js
@@ -34,38 +34,51 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 510, 0, 2500] }
 		],
 		"s-739-2000-113-0": [{ type: "text", sub_type: "message", message: "Stun", message_PT: "Stun", message_ES: "Stun" }],
-		"s-739-2000-108-0": [{ type: "text", sub_type: "message", message: "Clense", message_PT: "Cleanse", message_ES: "Cleanse", class_position: "heal" }],
+		"s-739-2000-108-0": [
+			{ type: "text", sub_type: "message", message: "Clense", message_PT: "Cleanse", message_ES: "Cleanse", class_position: "heal" },
+			{ type: "text", sub_type: "message", message: "Clense", message_PT: "Cleanse", message_ES: "Cleanse", class_position: ["tank", "dps"] }
+		],
 		"s-739-2000-115-0": [
 			{ type: "text", sub_type: "message", message: "Whirlwind", message_PT: "Redemoinho", message_ES: "Vortice" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 360, 0, 6500] }
 		],
 		"s-739-2000-119-0": [{ type: "text", sub_type: "message", message: "Front", message_PT: "Spray a Frente", message_ES: "Ataque Frontal" }],
 		"s-739-2000-120-0": [{ type: "text", sub_type: "message", message: "Back", message_PT: "Disparo de Canhao Atrás", message_ES: "Ataque Atrás" }],
-		"s-739-2000-303-0": [{ type: "text", sub_type: "message", message: "Whip", message_PT: "Chicote", message_ES: "Latigo" }],
+		"s-739-2000-303-0": [
+			{ type: "text", sub_type: "message", message: "Whip", message_PT: "Chicote", message_ES: "Latigo" },
+			{ type: "text", sub_type: "message", delay: 1600, message: "Iframe", message_PT: "Iframe", message_ES: "Iframe!" }
+		],
 
 		// 3 BOSS
 		"nd-739-3001": [
 			{ type: "stop_timers" },
 			{ type: "despawn_all" }
-		],		
+		],
+		"h-739-3001-30": [{ type: "text", sub_type: "message", message: "Reveal soon...", message_PT: "Revealar em Breve...", message_ES: "Revelar Pronto..." }],
 		"s-739-3001-201-0": [
-			{ type: "text", sub_type: "message", message_PT: "Rugido Forte (Stun)", message_ES: "Rugido Fuerte (Stun)", message: "Strong Roar (Stun)" },
-			{ type: "text", sub_type: "message", delay: 3700, message_PT: "Iframe", message_ES: "Iframe", message: "Iframe!" }	
+			{ type: "text", sub_type: "message", message: "Strong Roar (Stun)", message_PT: "Rugido Forte (Stun)", message_ES: "Rugido Fuerte (Stun)" },
+			{ type: "text", sub_type: "message", message: "Iframe!", message_PT: "Iframe", message_ES: "Iframe", delay: 3700 },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 630, 0, 4000] }
 		],	
-		"s-739-3001-107-0": [{ type: "text", sub_type: "message", message_PT: "Muitos Hits (Garras)", message_ES: "Muchos Hits (Garras)", message: "Many Hits" }],
+		"s-739-3001-107-0": [{ type: "text", sub_type: "message", message: "Many Hits", message_PT: "Muitos Hits (Garras)", message_ES: "Muchos Hits (Garras)" }],
 		"s-739-3001-115-0": [
-			{ type: "text", sub_type: "message", message_PT: "Rugido (Stun)", message_ES: "Rugido (Stun)", message: "Roar (Stun)" },
-			{ type: "text", sub_type: "message", delay: 1800, message_PT: "Iframe", message_ES: "Iframe", message: "Iframe!" },
-			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 6, 630, 0, 4000] }
+			{ type: "text", sub_type: "message", message: "Roar (Stun)", message_PT: "Rugido (Stun)", message_ES: "Rugido (Stun)"},
+			{ type: "text", sub_type: "message", message: "Iframe!", message_PT: "Iframe", message_ES: "Iframe", Daley: 1800 },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 630, 0, 4000] }
 		],
-		"s-739-3001-118-0": [{ type: "text", sub_type: "message", message_PT: "Arranhar", message_ES: "Rasguno", message: "Scratching" }],
+		"s-739-3001-118-0": [{ type: "text", sub_type: "message", message: "Scratching", message_PT: "Arranhar", message_ES: "Rasguno"}],
 		// Revealed Argog
-		"s-739-3001-167-0": [{ type: "text", sub_type: "message", message_PT: "Muitos Hits (Garras)", message_ES: "Muchos Hits (Garras)", message: "Many Hits" }],
+		"s-739-3001-164-0": [{ type: "text", sub_type: "message", message: "Counter Attack (bleed)", message_PT: "Conta Ataque (Sangramento)", message_ES: "Conta Ataque (Sangramiento)" }],
+		"s-739-3001-167-0": [{ type: "text", sub_type: "message", message: "Many Hits", message_PT: "Muitos Hits (Garras)", message_ES: "Muchos Hits (Garras)"}],
 		"s-739-3001-175-0": [
-			{ type: "text", sub_type: "message", message_PT: "Rugido (Stun)", message_ES: "Rugido (Stun)", message: "Roar (Stun)" },
-			{ type: "text", sub_type: "message", delay: 1800, message_PT: "Iframe", message_ES: "Iframe", message: "Iframe!" },
+			{ type: "text", sub_type: "message", message: "Roar (Stun)", message_PT: "Rugido (Stun)", message_ES: "Rugido (Stun)" },
+			{ type: "text", sub_type: "message", message: "Iframe!", message_PT: "Iframe", message_ES: "Iframe", Daley: 1800 },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 6, 630, 0, 4000] }
-		],
-		"s-739-3001-178-0": [{ type: "text", sub_type: "message", message_PT: "Arranhar (Sangrar)", message_ES: "Rasguno (Sangrar)", message: "Scratching (bleed)" }]
+		],		
+		"s-739-3001-177-0": [{ type: "text", sub_type: "message", message: "Backstab", message_PT: "Ataque nas Costas", message_ES: "Ataque por la Espalda" }],
+		"s-739-3001-178-0": [{ type: "text", sub_type: "message", message: "Scratching (bleed)", message_PT: "Arranhar (Sangrar)", message_ES: "Rasguno (Sangrar)" }],
+		"s-739-3001-203-0": [{ type: "text", sub_type: "message", message: "Phantom x3 (bleed)", message_PT: "Fantasma x3 (Sangramento)", message_ES: "Fantasma x3 (Sangramiento)" }],
+		"s-739-3001-207-0": [{ type: "text", sub_type: "message", message: "Phantom x5 (bleed)", message_PT: "Fantasma x5 (Sangramento)", message_ES: "Fantasma x5 (Sangramiento)" }],
+		"s-739-3001-212-0": [{ type: "text", sub_type: "message", message: "Flash", message_PT: "Flash", message_ES: "Flash" }]
 	};
 };
--- guides/9939.js
+++ guides/9939.js
@@ -1,8 +1,10 @@
-// Red Refuge (Hard)
+// Red Refuge (Difícil)
 //
 // made by multarix
 
 module.exports = (dispatch, handlers, guide, lang) => {
+		let first_boss_debuff = false;
+		
 	return {
 		// 1 BOSS
 		"nd-939-1000": [
@@ -10,13 +12,29 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "despawn_all" }
 		],
 		"s-939-1000-105-0": [{ type: "text", sub_type: "message", message: "Turn + Breath", message_ES: "Girar + Respirar", message_PT: "Virar + Respirar" }],
+		"die": [{ type: "func", func: () => { first_boss_debuff = false; } }],
+		"ar-0-0-93910005": [{ type: "func", func: () => first_boss_debuff = false }],
+		"ae-0-0-93910005": [{ type: "func", func: () => first_boss_debuff = true }],
+		"s-939-93910-305-0": [
+			{ type: "text", sub_type: "message", message: "Enter the big circle", message_ES: "Entra en círculo Grande", message_PT: "Entrar no Círculo Grande", check_func: () => first_boss_debuff },
+			{ type: "text", sub_type: "message", message: "Exit the big circle", message_ES: "Salir de círculo Grande", message_PT: "Sair do círculo Grande", check_func: () => !first_boss_debuff }
+		],
+		"s-939-93911-305-0": "s-939-93910-305-0",
+		"s-939-93912-305-0": "s-939-93910-305-0",
+		"s-939-93913-305-0": "s-939-93910-305-0",
+		"s-939-93914-305-0": "s-939-93910-305-0",
 		"s-939-1000-308-0": [
 			{ type: "text", sub_type: "message", message: "In > Out", message_ES: "Entrar > Salir", message_PT: "Entrar > Salir" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 300, 0, 7500] }
 		],
 		"s-939-1000-308-1": [{ type: "text", sub_type: "message", message: "Out", message_ES: "Salir", message_PT: "Sair" }],
 		"s-939-1000-112-0": [{ type: "text", sub_type: "message", message: "Back Spray", message_ES: "Atrás Spray", message_PT: "Atrás Spray" }],
+		"s-939-1000-120-0": "s-939-1000-112-0",
+		"s-939-1000-113-0": [{ type: "text", sub_type: "message", message: "Back Spray + Fire Wave", message_ES: "Atrás Spray + Ola de Fuego", message_PT: "Atrás Spray + Onda de Fogo" }],
+		"s-939-1000-115-0": [{ type: "text", sub_type: "message", message: "Front Spray + Fire Wave", message_ES: "Frene Spray + Ola de Fuego", message_PT: "Frente Spray + Onda de Fogo" }],
 		"s-939-1000-107-0": [{ type: "text", sub_type: "message", message: "Jump", message_ES: "Salto", message_PT: "Salto" }],
+		"s-939-1000-117-0": [{ type: "text", sub_type: "message", message: "Jump + Spin", message_ES: "Salto + Giro", message_PT: "Salto + Giro" }],
+		"s-939-1000-119-0": [{ type: "text", sub_type: "message", message: "Many Hits", message_ES: "Muchos Golpes", message_PT: "Muchos Golpes" }],
 		"s-939-1000-306-0": [
 			{ type: "text", sub_type: "message", message: "Out > In", message_ES: "Salir > Entrar", message_PT: "Salir > Entrar" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 300, 0, 7500] }
@@ -34,14 +52,24 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 510, 0, 2500] }
 		],
 		"s-939-2000-113-0": [{ type: "text", sub_type: "message", message: "Stun", message_ES: "Stun", message_PT: "Stun" }],
-		"s-939-2000-108-0": [{ type: "text", sub_type: "message", message: "Clense", message_ES: "Clense", message_PT: "Clense", class_position: "heal" }],
+		"s-939-2000-108-0": [
+			{ type: "text", sub_type: "message", message: "Cleanse", message_ES: "Cleanse", message_PT: "Cleanse", class_position: "heal" },
+			{ type: "text", sub_type: "message", message: "Spin", message_ES: "Giro", message_PT: "Giro", class_position: ["tank", "dps"] }
+		],	
 		"s-939-2000-115-0": [
 			{ type: "text", sub_type: "message", message: "Whirlwind", message_ES: "Vortice", message_PT: "Redemoinho" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 360, 0, 6500] }
 		],
 		"s-939-2000-119-0": [{ type: "text", sub_type: "message", message: "Front", message_ES: "Frente", message_PT: "Frente" }],
 		"s-939-2000-120-0": [{ type: "text", sub_type: "message", message: "Back", message_ES: "Atrás", message_PT: "Atrás" }],
-		"s-939-2000-303-0": [{ type: "text", sub_type: "message", message: "Whip", message_ES: "Latigo", message_PT: "Chicote" }],
+		"s-939-2000-303-0": [
+			{ type: "text", sub_type: "message", message: "Whip", message_ES: "Latigo", message_PT: "Chicote" },
+			{ type: "text", sub_type: "message", message: "Dodge", message_ES: "Iframe", message_PT: "Iframe", delay: 1600 }
+		],
+		"s-939-2000-304-0": [
+			{ type: "text", sub_type: "message", message: "Pull", message_ES: "Jalar", message_PT: "Puxar" },
+			{ type: "text", sub_type: "message", message: "Dodge", message_ES: "Iframe", message_PT: "Iframe", delay: 1600 }
+		],
 
 		// 3 BOSS
 		"nd-939-3001": [
@@ -49,19 +77,31 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "despawn_all" }
 		],
 		"h-939-3001-30": [{ type: "text", sub_type: "message", message: "Reveal soon...", message_ES: "Revelar Pronto...", message_PT: "Revelar em Breve..." }],
-		"s-939-3001-201-0": [{ type: "text", sub_type: "message", delay: 3600, message: "Dodge!", message_ES: "Iframe!", message_PT: "Iframe!" }],
+		"s-939-3001-201-0": [
+			{ type: "text", sub_type: "message", message: "Strong Roar (Stun)", message_ES: "Rugido Fuerte (Stun)", message_PT: "Rugido Forte (Stun)" },
+			{ type: "text", sub_type: "message", delay: 3600, message: "Dodge!", message_ES: "Iframe!", message_PT: "Iframe!" },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 630, 0, 4000] }
+		],
 		"s-939-3001-107-0": [{ type: "text", sub_type: "message", message: "Many Hits", message_ES: "Muchos Golpes", message_PT: "Muitos Golpes" }],
 		"s-939-3001-115-0": [
+			{ type: "text", sub_type: "message", message: "Strong Roar (Stun)", message_PT: "Rugido Fuerte (Stun)", message_PT: "Rugido Forte (Stun)" },
 			{ type: "text", sub_type: "message", delay: 1750, message: "Dodge!", message_ES: "Iframe", message_PT: "Iframe!" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 630, 0, 4000] }
 		],
 		"s-939-3001-118-0": [{ type: "text", sub_type: "message", message: "Scratching", message_ES: "Rasguno", message_PT: "Arranhar" }],
 		// Revealed Argog
+		"s-939-3001-164-0": [{ type: "text", sub_type: "message", message: "Counter Attack (bleed)", message_ES: "Conta Ataque (Sangramiento", message_PT: "Conta Ataque (Sangramento)" }],
 		"s-939-3001-167-0": [{ type: "text", sub_type: "message", message: "Many Hits", message_ES: "Muchos Golpes", message_PT: "Muitos Golpes" }],
+		"s-939-3001-172-0": [{ type: "text", sub_type: "message", message: "Target", message_ES: "Objetivo", message_PT: "Alvo" }],
 		"s-939-3001-175-0": [
+			{ type: "text", sub_type: "message", message: "Strong Roar (Stun)", message_PT: "Rugido Fuerte (Stun)", message_PT: "Rugido Forte (Stun)" },
 			{ type: "text", sub_type: "message", delay: 1750, message: "Dodge!", message_ES: "Iframe!", message_PT: "Iframe!" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 630, 0, 4000] }
 		],
-		"s-939-3001-178-0": [{ type: "text", sub_type: "message", message: "Scratching (bleed)", message_ES: "Rasguno (Sangrar)", message_PT: "Arranhar (Sangrar)" }]
+		"s-939-3001-177-0": [{ type: "text", sub_type: "message", message: "Backstab", message_ES: "Ataque por la Espalda", message_PT: "Ataque nas Costas" }],
+		"s-939-3001-178-0": [{ type: "text", sub_type: "message", message: "Scratching (bleed)", message_ES: "Rasguno (Sangrar)", message_PT: "Arranhar (Sangrar)" }],
+		"s-939-3001-203-0": [{ type: "text", sub_type: "message", message: "Phantom x3 (bleed)", message_ES: "Fantasma x3 (Sangramiento)", message_PT: "Fantasma x3 (Sangramento)" }],
+		"s-939-3001-207-0": [{ type: "text", sub_type: "message", message: "Phantom x5 (bleed)", message_ES: "Fantasma x5 (Sangramiento)", message_PT: "Fantasma x5 (Sangramento)" }],
+		"s-939-3001-212-0": [{ type: "text", sub_type: "message", message: "Flash", message_ES: "Flash", message_PT: "Flash" }]
 	};
 };
