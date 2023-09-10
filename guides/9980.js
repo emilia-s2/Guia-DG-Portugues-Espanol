25fe620942bdf138c48d3cd791ac99db1598c0ae
Emilia <emilia.sz_@hotmail.com>
2023-09-10 20:05:05 -0300

    Update VHH (10/09/23)

--- guides/9980.js
+++ guides/9980.js
@@ -34,14 +34,18 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-980-2000-105-0": [{ type: "text", sub_type: "message", message: "Heavy Front Attack", message_ES: "Ataque Frontal Fuerte", message_PT: "Ataque Frontal Forte" }],
 		"s-980-2000-106-0": [{ type: "text", sub_type: "message", message: "Flame Ray (Target)", message_ES: "Rayo de Fuego (Objetivo)", message_PT: "Raio de Fogo (Alvo)" }],
 		"s-980-2000-106-1": [{ type: "text", sub_type: "message", message: "Dodge", message_ES: "Iframe", message_PT: "Iframe", delay: 200 }],
-		"s-980-2000-107-0": [{ type: "text", sub_type: "message", message: "Whip", message_ES: "Látigo", message_PT: "Chicote" }],
+		"s-980-2000-107-0": [{ type: "text", sub_type: "message", message: "Whip", message_ES: "Látigo", message_PT: "Chicote" },
+			{ type: "spawn", func: "vector", args: [912, 360, 50, 0, 900, 0, 2000] }
+		],
 		"s-980-2000-107-1": [{ type: "text", sub_type: "message", message: "Dodge", message_ES: "Iframe", message_PT: "Iframe", delay: 200 }],
 		"s-980-2000-108-0": [{ type: "text", sub_type: "message", message: "Front Attack (Stun)", message_ES: "Ataque Frontal (Stun)", message_PT: "Ataque Frontal (Stun)" }],
 		"s-980-2000-301-0": [{ type: "text", sub_type: "message", message: "Jump (Target)", message_ES: "Salto (Objetivo)", message_PT: "Salto (Alvo)" }],
 		"s-980-2000-301-1": [{ type: "text", sub_type: "message", message: "Dodge", message_ES: "Iframe", message_PT: "Iframe", delay: 400 }],
 		"s-980-2000-302-0": [{ type: "text", sub_type: "message", message: "AOE (Stand behind a stone)", message_ES: "AOE (Párate Detrás de una Piedra)", message_PT: "AOE (Fique Atrás de uma Pedra)" }],
 		"s-980-2000-303-0": [{ type: "text", sub_type: "message", message: "Circles", message_ES: "Círculos", message_PT: "Círculos" }],
-		"s-980-2000-304-0": [{ type: "text", sub_type: "message", message: "Jump", message_ES: "Salto", message_PT: "Salto" }],
+		"s-980-2000-304-0": [{ type: "text", sub_type: "message", message: "Jump", message_ES: "Salto", message_PT: "Salto" },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 270, 0, 4000] }
+		],
 		"s-980-2000-304-1": [{ type: "text", sub_type: "message", message: "Dodge", message_ES: "Iframe", message_PT: "Iframe", delay: 400 }],
 		"s-980-2001-326-0": [{ type: "spawn", func: "marker", args: [false, 180, 250, 0, 6000, true, null] }], // stone marker
 
@@ -58,7 +62,10 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-980-3000-112-2": [{ type: "text", sub_type: "message", message: "Front | Back", message_ES: "Ataque Frontal | Atrás", message_PT: "Frente | Atrás" }],
 		"s-980-3000-113-0": [{ type: "text", sub_type: "message", message: "Pushback", message_ES: "Empujar", message_PT: "Empurrar" }],
 		"s-980-3000-114-0": "s-980-3000-113-0",
-		"s-980-3000-115-0": [{ type: "text", sub_type: "message", message: "Charging", message_ES: "Cargando", message_PT: "Carregando" }],
+		"s-980-3000-115-0": [{ type: "text", sub_type: "message", message: "Charging", essage_ES: "Cargando", message_PT: "Carregando" },
+			{ type: "text", sub_type: "message", message: "Plague of Exhaustion", message_ES: "Plague of Exhaustion", message_PT: "Plague of Exhaustion", class_position: "priest" },
+			{ type: "text", sub_type: "message", message: "Regression", message_ES: "Regression", message_PT: "Regression", class_position: "mystic" }
+		],
 		"s-980-3000-301-0": [{ type: "text", sub_type: "message", message: "Get Out | Get In", message_ES: "Salir| Entrar", message_PT: "Sair | Entrar" }],
 		"s-980-3000-302-0": [{ type: "text", sub_type: "message", message: "Get In", message_ES: "Entrar", message_PT: "Entrar"}],
 		"s-980-3000-304-0": [{ type: "text", sub_type: "message", message: "Shot (Target)", message_ES: "Disparar (Objetivo)", message_PT: "Tiros (Alvo)" }],
