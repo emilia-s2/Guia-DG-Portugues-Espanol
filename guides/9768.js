042051b6afd0976e8b8fcb08705af046a029bd3a
Emilia <emilia.sz_@hotmail.com>
2023-12-24 20:59:57 -0300

    Update Shadow Sanguinary Normal/Hard 24/12/23

--- guides/9768.js
+++ guides/9768.js
@@ -7,6 +7,8 @@ module.exports = (dispatch, handlers, guide, lang) => {
 	let thirdboss_print_combo = true;
 	let thirdboss_print_bait = true;
 	let thirdboss_combo_count = 0;
+	let thirdboss_combo_last_128 = null;
+	let thirdboss_combo_last_129 = null;
 
 	function secondboss_floor_event(one, two) {
 		if (one && two) {
@@ -89,9 +91,22 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-768-3000-102-0": "s-768-3000-101-0",
 		//
-		"s-768-3000-130-0": [
-			{ type: "text", sub_type: "message", message: "Left", message_ES: "Izquierda", message_PT: "Esquerda" },
-			{ type: "func", func: () => thirdboss_combo_count++ }
+		"s-768-3000-128-0": [ // 128 -> 106/130
+			{ type: "text", sub_type: "message", message: "Back/Left", message_ES: "Atrás/Izquierda", message_PT: "Atrás Esquerda", check_func: () => thirdboss_combo_last_128 === null },
+			{ type: "text", sub_type: "message", message: "Back", message_ES: "Atrás", message_PT: "Atrás", check_func: () => thirdboss_combo_last_128 === 130 },
+			{ type: "text", sub_type: "message", message: "Left", message_ES: "Izquierda", message_PT: "Esquerda", check_func: () => thirdboss_combo_last_128 === 106 }
+		],
+		"s-768-3000-129-0": [ // 129 -> 108/131
+			{ type: "text", sub_type: "message", message: "Front/Right", message_ES: "Frente/Derecha", message_PT: "Frente/Direita", check_func: () => thirdboss_combo_last_129 === null },
+			{ type: "text", sub_type: "message", message: "Front", message_ES: "Frente", message_PT: "Frente", check_func: () => thirdboss_combo_last_129 === 131 },
+			{ type: "text", sub_type: "message", message: "Right", message_ES: "Derecha", message_PT: "Direita", check_func: () => thirdboss_combo_last_129 === 108 }
+		],
+		"s-768-3000-130-0": [ // 128 -> 130
+			{ type: "text", sub_type: "message", message: "Left", message_ES: "Izquierda", message_PT: "Esquerda", check_func: () => thirdboss_combo_last_128 === null },
+			{ type: "func", func: () => {
+				thirdboss_combo_count++;
+				thirdboss_combo_last_128 = 130;
+			} }
 		],
 		"s-768-3000-105-0": [ // 130 -> 105
 			{ type: "spawn", func: "vector", args: [553, 0, 0, 180, 500, 0, 1500] },
@@ -102,13 +117,19 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "semicircle", args: [180, 360, 912, 0, 0, 8, 360, 0, 1500] }
 		],
 		"s-768-3000-106-0": [ // 128 -> 106
-			{ type: "text", sub_type: "message", message: "Back", message_ES: "Atrás", message_PT: "Atrás" },
+			{ type: "text", sub_type: "message", message: "Back", message_ES: "Atrás", message_PT: "Atrás", check_func: () => thirdboss_combo_last_128 === null },
 			{ type: "spawn", func: "circle", args: [false, 553, 180, 340, 12, 270, 0, 2600] },
-			{ type: "func", func: () => thirdboss_combo_count++ }
+			{ type: "func", func: () => {
+				thirdboss_combo_count++;
+				thirdboss_combo_last_128 = 106;
+			} }
 		],
-		"s-768-3000-131-0": [
-			{ type: "text", sub_type: "message", message: "Right", message_ES: "Derecha", message_PT: "Direita" },
-			{ type: "func", func: () => thirdboss_combo_count++ }
+		"s-768-3000-131-0": [ // 129 -> 131
+			{ type: "text", sub_type: "message", message: "Right", message_ES: "Derecha", message_PT: "Direita", check_func: () => thirdboss_combo_last_129 === null },
+			{ type: "func", func: () => {
+				thirdboss_combo_count++;
+				thirdboss_combo_last_129 = 131;
+			} }
 		],
 		"s-768-3000-107-0": [ // 131 -> 107
 			{ type: "spawn", func: "vector", args: [553, 0, 0, 180, 500, 0, 1500] },
@@ -118,9 +139,12 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "semicircle", args: [0, 180, 912, 0, 0, 10, 300, 0, 1500] },
 			{ type: "spawn", func: "semicircle", args: [0, 180, 912, 0, 0, 8, 360, 0, 1500] }
 		],
-		"s-768-3000-108-0": [ // 129 > 108
-			{ type: "text", sub_type: "message", message: "Front", message_ES: "Frente", message_PT: "Frente" },
-			{ type: "func", func: () => thirdboss_combo_count++ }
+		"s-768-3000-108-0": [ // 129 -> 108
+			{ type: "text", sub_type: "message", message: "Front", message_ES: "Frente", message_PT: "Frente", check_func: () => thirdboss_combo_last_129 === null },
+			{ type: "func", func: () => {
+				thirdboss_combo_count++;
+				thirdboss_combo_last_129 = 108;
+			} }
 		],
 		"s-768-3000-109-0": "s-768-3000-106-0",
 		//
@@ -144,7 +168,11 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-768-3000-206-0": [{ type: "text", sub_type: "message", message: "Pike (Target)", message_ES: "Lanza (Objetivo)", message_PT: "Lança (Alvo)" }], // 206 -> 207
 		"s-768-3000-302-0": [
 			{ type: "text", sub_type: "message", message: "Bait (Target)", message_ES: "Bait (Objetivo)", message_PT: "Bait (Alvo)" },
-			{ type: "func", func: () => thirdboss_combo_count = 0 }
+			{ type: "func", func: () => {
+				thirdboss_combo_count = 0;
+				thirdboss_combo_last_128 = null;
+				thirdboss_combo_last_129 = null;
+			} }
 		],
 		"s-768-3000-302-1": [{ type: "text", sub_type: "message", message: "Dodge", message_ES: "Iframe", message_PT: "Iframe", delay: 1600 }],
 		"s-768-3000-501-0": [
@@ -172,7 +200,11 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"give_bait_combo": [
 			{ type: "event", check_func: () => thirdboss_combo_count >= 3, args: [
 				{ type: "text", sub_type: "message", message: "Give Bait", message_ES: "Dar Bait", message_PT: "Dê Bait", class_position: "heal" },
-				{ type: "func", func: () => thirdboss_combo_count = 0 }
+				{ type: "func", func: () => {
+					thirdboss_combo_count = 0;
+					thirdboss_combo_last_128 = null;
+					thirdboss_combo_last_129 = null;
+				} }
 			] }
 		],
 		"e-768-3000-101": "give_bait",
